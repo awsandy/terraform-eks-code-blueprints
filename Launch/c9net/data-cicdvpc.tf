19c328d204472b395bc3bb6124e33cd69d38efd9
awsandy <awsandy@amazon.com>
2022-05-23 21:59:23 +0100

    setup

--- Launch/c9net/data-cicdvpc.tf
+++ Launch/c9net/data-cicdvpc.tf
@@ -1,9 +1,9 @@
 data "aws_vpc" "vpc-cicd" {
   default = false
-  vpc_id=data.terraform_remote_state.net.outputs.cicd-vpc
+  id=data.terraform_remote_state.net.outputs.cicd-vpc
 
-  filter {
-    name   = "tag:workshop"
-    values = ["eks-cicd"]
-  }
+  #filter {
+  #  name   = "tag:workshop"
+  #  values = ["eks-cicd"]
+  #}
 }
