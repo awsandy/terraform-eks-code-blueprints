54d40c83b09ab559bdbfd2f4094c301c0bd66506
awsandy <awsandy@amazon.com>
2022-05-26 12:54:37 +0100

    add ons

--- Launch/cluster/main.tf
+++ Launch/cluster/main.tf
@@ -17,6 +17,7 @@ locals {
 
 
 
+
 provider "kubernetes" {
   host                   = module.eks_blueprints.eks_cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks_blueprints.eks_cluster_certificate_authority_data)
@@ -29,6 +30,24 @@ provider "kubernetes" {
   }
 }
 
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks_blueprints.eks_cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks_blueprints.eks_cluster_certificate_authority_data)
+
+    exec {
+      api_version = "client.authentication.k8s.io/v1alpha1"
+      command     = "aws"
+      # This requires the awscli to be installed locally where Terraform is executed
+      args = ["eks", "get-token", "--cluster-name", module.eks_blueprints.eks_cluster_id]
+    }
+  }
+}
+
+
+
+
 module "eks_blueprints" {
   source = "github.com/aws-ia/terraform-aws-eks-blueprints?ref=v4.0.6"
 
@@ -36,6 +55,7 @@ module "eks_blueprints" {
   environment       = local.environment
   zone              = local.zone
   terraform_version = local.terraform_version
+
 
   # EKS Cluster VPC and Subnet mandatory config
   vpc_id  =  data.terraform_remote_state.net.outputs.eks-vpc
@@ -61,4 +81,50 @@ module "eks_blueprints" {
       subnet_ids      = data.terraform_remote_state.net.outputs.eks-priv-subnets
     }
   }
+
+  fargate_profiles = {
+    default = {
+      fargate_profile_name = "fargate1"
+      fargate_profile_namespaces = [
+        {
+          namespace = "fargate1"
+          k8s_labels = {
+            Environment = "preprod"
+            Zone        = "dev"
+            env         = "fargate"
+          }
+      }]
+      subnet_ids = data.terraform_remote_state.net.outputs.eks-priv-subnets
+      additional_tags = {
+        ExtraTag = "Fargate"
+      }
+    },
+  }
+
+
+
 }
+
+
+module "eks_blueprints_kubernetes_addons" {
+  source = "github.com/aws-ia/terraform-aws-eks-blueprints//modules/kubernetes-addons?ref=v4.0.6"
+
+  eks_cluster_id = module.eks_blueprints.eks_cluster_id
+
+  # EKS Addons
+  enable_amazon_eks_vpc_cni            = true
+  enable_amazon_eks_coredns            = true
+  enable_amazon_eks_kube_proxy         = true
+  enable_amazon_eks_aws_ebs_csi_driver = true
+
+  #K8s Add-ons
+  enable_argocd                       = true
+  enable_aws_for_fluentbit            = true
+  enable_aws_load_balancer_controller = true
+  enable_cluster_autoscaler           = false
+  enable_metrics_server               = true
+  enable_prometheus                   = true
+}
+
+
+
