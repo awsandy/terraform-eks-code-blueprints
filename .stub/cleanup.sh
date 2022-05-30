224ca7ee2949c9e7ebc1f44f0d43d3aef7873fb7
awsandy <awsandy@amazon.com>
2022-05-30 08:32:45 +0100

    up

--- .stub/cleanup.sh
+++ .stub/cleanup.sh
@@ -1,5 +1,5 @@
-rm -rf .terrform* backend.tf
-terraform init -no-color -migrate-state -force-copy > /dev/null
+rm -f  backend.tf
+terraform init -no-color -migrate-state -force-copy 
 terraform destroy -auto-approve -no-color 
 rc=$(terraform state list | wc -l)
 if [ $rc -gt 0 ];then
@@ -9,4 +9,5 @@ else
     echo "OK - $rc resources left, cleaning directory"
 fi
 rm -f terraform.tfstate* tfplan 
+rm -rf .terraform
 echo "Done"
